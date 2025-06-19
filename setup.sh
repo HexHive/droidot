2dc4183a0d04cce0256eb69857976eb583913a3a
philippmao <maophilipp@gmail.com>
2025-06-19 19:36:30 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-wget https://zenodo.org/records/15697750/files/com.termux.tar.gz?download=1
+wget https://zenodo.org/records/15697750/files/com.termux.tar.gz
 mv com.termux.tar.gz emulator/
-wget https://zenodo.org/records/15697750/files/emulator_dist.tar.gz?download=1
+wget https://zenodo.org/records/15697750/files/emulator_dist.tar.gz
+wget https://zenodo.org/records/15700199/files/target_APK.tar.gz
+tar xvf target_APK.tar.gz
 docker build . -t droidot
