285245f480d2d23c164d4bc7246b7a70e00f7c3a
philippmao <maophilipp@gmail.com>
2025-06-19 20:06:56 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-https://zenodo.org/records/15700387/files/com.termux.tar.gz
+wget https://zenodo.org/records/15700387/files/com.termux.tar.gz
 mv com.termux.tar.gz emulator/
 wget https://zenodo.org/records/15697750/files/emulator_dist.tar.gz
 wget https://zenodo.org/records/15700199/files/target_APK.tar.gz
