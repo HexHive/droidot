4c46f660e6594657c49ac49fe954137a4f0af383
cyanpencil <lucadb96@gmail.com>
2025-07-01 18:24:34 +0200

    fix set -eu

--- static_ae/setup.sh
+++ static_ae/setup.sh
@@ -1,6 +1,6 @@
-wget https://zenodo.org/records/15700199/files/target_APK.tar.gz
+wget https://zenodo.org/records/15784901/files/target_APK.tar.gz
 tar xvf target_APK.tar.gz 
-wget https://zenodo.org/records/15707278/files/zenodo.tar
+wget https://zenodo.org/records/15784901/files/zenodo.tar
 tar xvf zenodo.tar
 cp ../Dockerfile_static_ae ./Dockerfile
 cp ../run_rq1.sh ./run_rq1.sh
